c2071a47455f76da898669e9ec8e482bb3c82655
gonexwind <fikky.ardi15@gmail.com>
2022-01-08 09:22:35 +0700

    admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->roles == 'ADMIN') {
+            return $next($request);
+        }
+        return redirect('/');
+    }
+}
